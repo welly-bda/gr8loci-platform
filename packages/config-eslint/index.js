04c2249cae8d4a3acd598994bb18a2890eb75e62
welly-bda <dw2.marketing@gmail.com>
2026-04-22 23:47:41 -0300

    chore: add shared ESLint config package

---
+++ packages/config-eslint/index.js
@@ -0,0 +1,27 @@
+const js = require('@eslint/js')
+const tseslint = require('typescript-eslint')
+const react = require('eslint-plugin-react')
+const reactHooks = require('eslint-plugin-react-hooks')
+const prettier = require('eslint-config-prettier')
+const next = require('eslint-config-next')
+
+module.exports = [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+  prettier,
+]
